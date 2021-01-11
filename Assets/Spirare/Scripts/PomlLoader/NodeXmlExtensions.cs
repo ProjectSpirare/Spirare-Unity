0e79da2a17d34dc2f8f74114d5390845cdde0efa
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2021-01-11 13:26:06 +0900

    Separete nodexmlextensions

---
+++ Assets/Spirare/Scripts/PomlLoader/NodeXmlExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace Spirare
+{
+    internal static class XmlNodeExtensions
+    {
+        public static bool TryGetAttribute(this XmlNode node, string key, out string value)
+        {
+            var type = node.Attributes[key];
+            if (type == null)
+            {
+                value = null;
+                return false;
+            }
+
+            value = type.Value;
+            return true;
+        }
+
+        public static string GetAttribute(this XmlNode node, string key, string defaultValue = "")
+        {
+            if (node.TryGetAttribute(key, out var value))
+            {
+                return value;
+            }
+            return defaultValue;
+        }
+    }
+}
--- Assets/Spirare/Scripts/PomlLoader/PomlParser.cs
+++ Assets/Spirare/Scripts/PomlLoader/PomlParser.cs
@@ -280,29 +280,4 @@ namespace Spirare
         }
         */
     }
-
-    internal static class XmlNodeExtensions
-    {
-        public static bool TryGetAttribute(this XmlNode node, string key, out string value)
-        {
-            var type = node.Attributes[key];
-            if (type == null)
-            {
-                value = null;
-                return false;
-            }
-
-            value = type.Value;
-            return true;
-        }
-
-        public static string GetAttribute(this XmlNode node, string key, string defaultValue = "")
-        {
-            if (node.TryGetAttribute(key, out var value))
-            {
-                return value;
-            }
-            return defaultValue;
-        }
-    }
 }
