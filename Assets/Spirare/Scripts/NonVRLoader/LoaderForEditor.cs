533435a31db1f67f64457e752f976458c042c711
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2021-01-16 10:59:17 +0900

    add scripts

---
+++ Assets/Spirare/Scripts/NonVRLoader/LoaderForEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spirare
+{
+    public class LoaderForEditor : PomlLoader
+    {
+        protected override WasmBehaviour AttachScript(PomlScriptElement element, Transform parent)
+        {
+            var wasm = base.AttachScript(element, parent);
+
+            //var handler = gameObject.AddComponent<EventHandlerForNonVR>();
+           // handler.OnUse += wasm.InvokeOnUse;
+
+            return wasm;
+        }
+    }
+}
