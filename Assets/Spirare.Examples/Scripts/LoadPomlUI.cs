72c76ca5f54e07fdf70f90634936e2433ce86df1
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2021-01-16 18:22:22 +0900

    add sample scene

---
+++ Assets/Spirare.Examples/Scripts/LoadPomlUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Spirare.Examples
+{
+    public class LoadPomlUI : MonoBehaviour
+    {
+        //[SerializeField]
+        //WasmFromUrl wasmFromUrl = null;
+
+        [SerializeField]
+        private Button button = null;
+
+        [SerializeField]
+        private InputField inputField = null;
+
+        void Awake()
+        {
+            button.onClick.AddListener(() =>
+            {
+                var url = inputField.text;
+                _ = Load(url);
+                //_ = wasmFromUrl.LoadWasmFromUrl(url);
+            });
+        }
+
+        private async Task Load(string url)
+        {
+            var pomlLoader = new GameObject();
+            var loader = pomlLoader.AddComponent<PomlLoader>();
+            await loader.LoadAsync(url);
+        }
+    }
+}
